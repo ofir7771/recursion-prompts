4623012f5ecee5c718d5582be16d5b06bcf61353
OFIR ALPERSHTEIN <ofiralpershtein@OFIRs-MacBook-Pro.local>
2020-09-16 15:28:08 -0600

    problems 5 -11

--- src/recursion.js
+++ src/recursion.js
@@ -75,6 +75,15 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  if (x === y || x === y+1 || x === y-1) {
+    return [];
+  }
+  if (x > y+1) {
+    return [x-1].concat(range(x-1, y));
+  }
+  if (x < y-1) {
+    return [x+1].concat(range(x+1, y));
+  }
 };
 
 // 7. Compute the exponent of a number.
